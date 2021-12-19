530c7d76d6345564689dfbeb0a3dcdff56c45c13
sanjeetaT <sanjeetadthapa@gmail.com>
2021-12-19 12:48:08 -0800

    Console App parts 1 to 6

---
+++ ConsoleAppPart1/ConsoleAppPart1.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPart1.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create one dimensional Array of string
+            string[] names = { "Ale", "Bed", "Cali", "Joy", "Alex" };
+            //Ask the user input some text
+            Console.WriteLine("Plese put the some test");
+            string userText = Console.ReadLine();
+
+            // A loop that iterate through each string
+            for (int i = 0; i < names.Length; i++)
+            {
+                //names[i] = names[i] + userText;
+                names[i] += " " + userText;
+            }
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
