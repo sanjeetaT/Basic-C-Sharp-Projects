36fd9bbcd3a596088ed752474e7ccdd23b515e2e
sanjeetaT <sanjeetadthapa@gmail.com>
2021-12-20 12:13:53 -0800

    Console app string and integer

---
+++ ConsoleAppStringAndIntegerAssignment/ConsoleAppStringAndIntegerAssignment.cs/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStringAndIntegerAssignment.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create a List of the integer
+            List<int> numbers = new List<int>{2,4,6,8,10};
+            //Ask the user for number to divided each number
+            Console.WriteLine("Plese put the number to divided each number");
+            int user = Convert.ToInt32(Console.ReadLine());
+
+            // A loop that iterate through each string
+          
+          
+            try
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+
+                    int result = numbers[i] / user;
+                    Console.WriteLine(result);
+
+                }
+
+            }
+
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("Please don't divided by zero");
+            //}
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("We finished the try catch");
+                Console.ReadLine();
+            }
+        }
+    }
+}
