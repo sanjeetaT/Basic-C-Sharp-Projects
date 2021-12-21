f28101d62acdc9320de962a2db9131a885d4e322
sanjeetaT <sanjeetadthapa@gmail.com>
2021-12-20 22:08:46 -0800

    Console part 6

--- ConsoleAppPart6/ConsoleAppPart6.cs/Program.cs
+++ ConsoleAppPart6/ConsoleAppPart6.cs/Program.cs
@@ -13,13 +13,14 @@ namespace ConsoleAppPart6.cs
             // Create the list of string that has at least two identical string in the list
             List<string> vowels = new List<string> { "a", "e", "i", "o", "u", "a" };
             List<string> vowelsCopy = new List<string> ();
-
+            // Create the for each loop that evalute each item in the list
             foreach (string vowel in vowels)
             {
              foreach(string vowelCopy in vowelsCopy)
                 {
                     if(vowel == vowelCopy)
                     {
+                        // display the message that string has already appear
                         Console.WriteLine(vowel + " has Appeared Before");
                     }
                 }
