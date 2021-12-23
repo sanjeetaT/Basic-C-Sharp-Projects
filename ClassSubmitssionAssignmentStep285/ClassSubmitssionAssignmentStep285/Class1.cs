0ba9de6d6753598ab60535b0a1aae7ef5f94d159
sanjeetaT <sanjeetadthapa@gmail.com>
2021-12-22 22:13:09 -0800

    Submission Assignment

---
+++ ClassSubmitssionAssignmentStep285/ClassSubmitssionAssignmentStep285/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmitssionAssignmentStep285
+{
+    class Class1
+    {
+        // create the void method that output an integer
+        //  Have a method divided the data passed to it by 2
+        public void divided(int a)
+        {
+            int result = a / 2;
+            Console.WriteLine("Here is " + a + " / 2  = " + result);
+
+
+        }
+
+        public static void divided( int num1, out int result )
+        {
+            result = num1;
+        }
+
+    }
+}
---
+++ ClassSubmitssionAssignmentStep285/ClassSubmitssionAssignmentStep285/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmitssionAssignmentStep285
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1();
+            int result = 0;
+            // ask the user put the number
+            Console.WriteLine("input a number you want divided by 2");
+            int a = Convert.ToInt32(Console.ReadLine());
+            obj1.divided(a);
+            Console.WriteLine("this is the using the out parameter");
+            int number = 7;
+            Console.WriteLine("result is now " + result);
+            Class1.divided(number, out result);
+            Console.WriteLine("result is now" + result);
+            Console.ReadLine();
+        }
+    }
+}
