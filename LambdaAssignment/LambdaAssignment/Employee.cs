5ab2fb27171f63c93fc07f6de616e5cd4852a244
sanjeetaT <sanjeetadthapa@gmail.com>
2021-12-24 15:11:59 -0800

    lambda Assignment

---
+++ LambdaAssignment/LambdaAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAssignment
+{
+    class Employee
+    {
+        public List<string> FirstName { get; set; }
+        public List<string> LastName { get; set; }
+
+        public List<int> ID { get; set; }
+    }
+}
---
+++ LambdaAssignment/LambdaAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = new List<string>()
+            {
+                "Joe", "Alex", "RO", "Joe", "Yo", "Sam","Char", "Bed", "YO", "Kat"
+            };
+            employee.LastName = new List<string>()
+            {
+              "Roy", "Morgen", "Lang", "Dahal","La", "Gi", "Koper", "Khan", "Kumar","Rai"
+            };
+            employee.ID = new List<int>()
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+            List<string> Joe = employee.FirstName.Where(x => x == "Joe").ToList();
+            List<int> moreThan5 = employee.ID.Where(X => X > 5).ToList();
+            //List<Employee> employee = new List<Employee>();
+            //List<string> FirstName = new List<string>();
+            //List<string> LaststName = new List<string>();
+             //  List<int> ID = new List<int>();
+
+            foreach(string firstName in Joe)
+            {
+                Console.WriteLine(firstName);
+            }
+            foreach (int  id in moreThan5)
+            {
+                Console.WriteLine(employee.FirstName[id]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
