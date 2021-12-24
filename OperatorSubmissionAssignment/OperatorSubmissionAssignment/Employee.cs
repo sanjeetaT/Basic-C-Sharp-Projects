b83d30018bcdcacd2a96af462681caa939d25c5f
sanjeetaT <sanjeetadthapa@gmail.com>
2021-12-23 17:49:02 -0800

    operator submittion

---
+++ OperatorSubmissionAssignment/OperatorSubmissionAssignment/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorSubmissionAssignment
+{
+    class Employee
+    {
+        // person class property
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int ID { get; set;}
+
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            return emp1.ID == emp2.ID;
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return emp1.ID != emp2.ID;
+        }
+    }
+}
---
+++ OperatorSubmissionAssignment/OperatorSubmissionAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { ID = 1 };
+            Employee employee2 = new Employee() { ID = 1 };
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+        }
+    }
+}
