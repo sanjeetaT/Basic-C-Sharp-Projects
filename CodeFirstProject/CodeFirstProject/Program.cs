72207c53a1d64370d83deea54c20f4dfdeb62570
sanjeetaT <sanjeetadthapa@gmail.com>
2022-01-05 14:57:35 -0800

    CodeFirst

---
+++ CodeFirstProject/CodeFirstProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CodeFirstProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var db = new StudentContext())
+            {
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Student { Name = name };
+                db.students.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database
+                var query = from b in db.students
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+           
+        
+
+
+
+
+
+
+    }
+    
+}
+
+    
+    
+
---
+++ CodeFirstProject/CodeFirstProject/StudentsContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstProject
+{
+    
+        public class StudentContext: DbContext
+        {
+            public DbSet<Student> students { get; set; }
+            
+        }
+    
+}
